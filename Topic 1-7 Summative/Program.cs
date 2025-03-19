2caa72d4d5548ac8b8ae345695433ad511019958
Linkin Krysa <339742660@GoTVDSB.ca>
2025-03-19 14:39:00 -0400

    Almost done with the betting then have to fix the bug if you can where it doesnt crash when you enter letters and not numbers.

--- Topic 1-7 Summative/Program.cs
+++ Topic 1-7 Summative/Program.cs
@@ -7,9 +7,10 @@ namespace Topic_1_7_Summative
         static void Main(string[] args)
         {
             Random generator = new Random();
-            int randNum, wallet, userBet;
+            int randNum, wallet, bet, gameEnd;
             string guess, choise;
             bool done = false;
+            bool doneBet = false;
 
             Console.WriteLine("Hello and welcome to my rock paper scissors game! :)");
             Console.WriteLine();
@@ -34,36 +35,74 @@ namespace Topic_1_7_Summative
             
             while (!done)
             {
-                wallet = 5;
+                gameEnd = 0;
+                wallet = 15;
                 randNum = generator.Next(1, 4);
                 Console.WriteLine();
                 Console.WriteLine("The computer has made it's choice.");
                 Console.WriteLine();
                 Console.Write("Now what is your choice (rock, paper or scissors)? ");
+                guess = Console.ReadLine().ToLower();
                 // 1 - Rock
                 // 2 - Paper
                 // 3 = Scissors
-                guess = Console.ReadLine().ToLower();
+                Console.ReadLine();
                 Console.WriteLine();
+                Console.Write("Now enter a bet from 1 to 5: ");
+                bet = Convert.ToInt32(Console.ReadLine());
+                doneBet = true;
+                Console.WriteLine();
+
+                while (!doneBet)
+                {
+                    Console.WriteLine();
+                    Console.Write("Now enter a bet from 1 to 5: ");
+                    bet = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+
+                    if (bet <= wallet)
+                    {
+                        doneBet = true;
+                    }
+                    else if (bet > wallet)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("You bet more than you have.");
+                        bet = wallet;
+                    }
+                    else if (bet < 0)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("You can't bet a negative number, making bet 0.");
+                        bet = 0;  
+                    }
+                }
 
                 if (guess == "paper")
                 {
                     if (randNum == 1)
                     {
                         Console.WriteLine("The computer chose Rock!!");
+                        Console.WriteLine();
                         Console.WriteLine("You won! As we know, paper beats rock.");
-                        wallet = wallet + 1;
+                        Console.WriteLine();
+                        Console.WriteLine("You won your bet back which was " + bet + " Dollars");
+                        wallet = wallet + bet;
                     }
                     else if (randNum == 2)
                     {
                         Console.WriteLine("The computer chose paper");
+                        Console.WriteLine();
                         Console.WriteLine("You tied the computer, As we know, paper can't beat paper.");
                     }
                     else if (randNum == 3)
                     {
                         Console.WriteLine("The computer chose scissors");
+                        Console.WriteLine();
                         Console.WriteLine("You lose! As we know, scissors beats paper.");
-                        wallet = wallet - 1;
+                        Console.WriteLine();
+                        Console.WriteLine("You lost your bet which was " + bet + " Dollars");
+                        wallet = wallet - bet;
                     }
                 }
                 else if (guess == "rock")
@@ -71,19 +110,26 @@ namespace Topic_1_7_Summative
                     if (randNum == 1)
                     {
                         Console.WriteLine("The computer chose rock!!");
+                        Console.WriteLine();
                         Console.WriteLine("You tied! As we know rock can't beat rock.");
                     }
                     else if (randNum == 2)
                     {
                         Console.WriteLine("The computer chose paper!!");
+                        Console.WriteLine();
                         Console.WriteLine("You lose! As we know, paper beats rock.");
-                        wallet = wallet - 1;
+                        Console.WriteLine();
+                        Console.WriteLine("You lost your bet which was " + bet + " Dollars");
+                        wallet = wallet - bet;
                     }
                     else if (randNum == 3)
                     {
                         Console.WriteLine("The computer chose scissors!!");
+                        Console.WriteLine();
                         Console.WriteLine("You won! As we know, rock beats scissors.");
-                        wallet = wallet + 1;
+                        Console.WriteLine();
+                        Console.WriteLine("You won your bet back which was " + bet + " Dollars");
+                        wallet = wallet + bet;
                     }
                 }
                 else if (guess == "scissors")
@@ -91,18 +137,26 @@ namespace Topic_1_7_Summative
                     if (randNum == 1)
                     {
                         Console.WriteLine("The computer chose rock!!");
+                        Console.WriteLine();
                         Console.WriteLine("You lose! As we know, rock beats scissors.");
-                        wallet = wallet - 1;
+                        Console.WriteLine();
+                        Console.WriteLine("You lost your bet which was " + bet + " Dollars");
+                        wallet = wallet - bet;
+
                     }
                     else if (randNum == 2)
                     {
                         Console.WriteLine("The computer chose paper!!");
+                        Console.WriteLine();
                         Console.WriteLine("You won! As we know, scissors beats paper.");
-                        wallet = wallet + 1;
+                        Console.WriteLine();
+                        Console.WriteLine("You won your bet back which was " + bet + " Dollars");
+                        wallet = wallet + bet;
                     }
                     else if (randNum == 3)
                     {
                         Console.WriteLine("The computer chose scissors!!");
+                        Console.WriteLine();
                         Console.WriteLine("You tied! As we know, scissors can't beat scissors.");
                     }
                 }
