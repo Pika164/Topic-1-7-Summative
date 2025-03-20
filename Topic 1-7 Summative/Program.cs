1a591b8f04b14a4d4402d248484f342415eb57e1
Linkin Krysa <339742660@GoTVDSB.ca>
2025-03-20 14:41:56 -0400

    finshed game but want to fix bugs if any.

--- Topic 1-7 Summative/Program.cs
+++ Topic 1-7 Summative/Program.cs
@@ -7,10 +7,15 @@ namespace Topic_1_7_Summative
         static void Main(string[] args)
         {
             Random generator = new Random();
-            int randNum, wallet, bet, gameEnd;
+            int randNum, wallet, bet = 0;
             string guess, choise;
             bool done = false;
             bool doneBet = false;
+            
+            Console.CursorVisible = false;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
 
             Console.WriteLine("Hello and welcome to my rock paper scissors game! :)");
             Console.WriteLine();
@@ -19,9 +24,9 @@ namespace Topic_1_7_Summative
             Console.Clear();
             Console.WriteLine("This will include standard rules for rock paper scissors.");
             Console.WriteLine();
-            Console.WriteLine("You will go against a computer and you will bet a certain amount each round and if you lose you get nothing but if you win you get what you bet back and if you tie, you dont lose or gain what you bet.");
+            Console.WriteLine("You will go against a computer and you will bet a certain amount each round and if you lose you get nothing but if you win you get what you bet back and if you tie, you dont lose or gain anything.");
             Console.WriteLine();
-            Console.WriteLine("You will start with 5 dollars in your wallet to start and you bet 1 dollar per round. You can't bet nothing and you lose when your wallet hits 0.");
+            Console.WriteLine("You will start with 15 dollars in your wallet to start and you bet 1 dollar per round all they way to the max money you have. You can't bet nothing and you lose when your wallet hits 0.");
             Console.WriteLine();
             Console.WriteLine("Now without more delay, lets start the game");
             Console.WriteLine();
@@ -29,16 +34,38 @@ namespace Topic_1_7_Summative
             Console.ReadLine();
             Console.Clear();
             //Round 1 Below
-            Console.WriteLine("let the games begin!:");
-            Console.WriteLine("==============");
             Console.WriteLine();
-            
+            wallet = 20;
+
             while (!done)
             {
-                gameEnd = 0;
-                wallet = 15;
                 randNum = generator.Next(1, 4);
-                Console.WriteLine();
+                doneBet = false;
+                while (!doneBet)
+                {
+                    Console.WriteLine("let the games begin!:");
+                    Console.WriteLine("==============");
+                    Console.WriteLine();
+                    Console.WriteLine("Your balance that you currently have is " + wallet);
+
+                    Console.WriteLine();
+                    Console.Write("Now enter a bet from 1 to " + wallet + " : ");
+                    bet = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+
+                    if (bet > wallet)
+                    {
+                        Console.WriteLine("You bet more than you have.");
+                    }
+                    else if (bet < 0)
+                    {
+                        Console.WriteLine("You can't bet a negative number, making bet 0.");
+                    }
+                    else
+                    {
+                        doneBet = true;
+                    }
+                }
                 Console.WriteLine("The computer has made it's choice.");
                 Console.WriteLine();
                 Console.Write("Now what is your choice (rock, paper or scissors)? ");
@@ -46,38 +73,8 @@ namespace Topic_1_7_Summative
                 // 1 - Rock
                 // 2 - Paper
                 // 3 = Scissors
-                Console.ReadLine();
                 Console.WriteLine();
-                Console.Write("Now enter a bet from 1 to 5: ");
-                bet = Convert.ToInt32(Console.ReadLine());
-                doneBet = true;
-                Console.WriteLine();
-
-                while (!doneBet)
-                {
-                    Console.WriteLine();
-                    Console.Write("Now enter a bet from 1 to 5: ");
-                    bet = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine();
-
-                    if (bet <= wallet)
-                    {
-                        doneBet = true;
-                    }
-                    else if (bet > wallet)
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine("You bet more than you have.");
-                        bet = wallet;
-                    }
-                    else if (bet < 0)
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine("You can't bet a negative number, making bet 0.");
-                        bet = 0;  
-                    }
-                }
-
+                
                 if (guess == "paper")
                 {
                     if (randNum == 1)
@@ -86,7 +83,7 @@ namespace Topic_1_7_Summative
                         Console.WriteLine();
                         Console.WriteLine("You won! As we know, paper beats rock.");
                         Console.WriteLine();
-                        Console.WriteLine("You won your bet back which was " + bet + " Dollars");
+                        Console.WriteLine("And you won your bet!");
                         wallet = wallet + bet;
                     }
                     else if (randNum == 2)
@@ -101,7 +98,7 @@ namespace Topic_1_7_Summative
                         Console.WriteLine();
                         Console.WriteLine("You lose! As we know, scissors beats paper.");
                         Console.WriteLine();
-                        Console.WriteLine("You lost your bet which was " + bet + " Dollars");
+                        Console.WriteLine("You lost your bet!");
                         wallet = wallet - bet;
                     }
                 }
@@ -119,7 +116,7 @@ namespace Topic_1_7_Summative
                         Console.WriteLine();
                         Console.WriteLine("You lose! As we know, paper beats rock.");
                         Console.WriteLine();
-                        Console.WriteLine("You lost your bet which was " + bet + " Dollars");
+                        Console.WriteLine("You lost your bet!");
                         wallet = wallet - bet;
                     }
                     else if (randNum == 3)
@@ -128,7 +125,7 @@ namespace Topic_1_7_Summative
                         Console.WriteLine();
                         Console.WriteLine("You won! As we know, rock beats scissors.");
                         Console.WriteLine();
-                        Console.WriteLine("You won your bet back which was " + bet + " Dollars");
+                        Console.WriteLine("You won your bet!");
                         wallet = wallet + bet;
                     }
                 }
@@ -140,7 +137,7 @@ namespace Topic_1_7_Summative
                         Console.WriteLine();
                         Console.WriteLine("You lose! As we know, rock beats scissors.");
                         Console.WriteLine();
-                        Console.WriteLine("You lost your bet which was " + bet + " Dollars");
+                        Console.WriteLine("You lost your bet!");
                         wallet = wallet - bet;
 
                     }
@@ -150,7 +147,7 @@ namespace Topic_1_7_Summative
                         Console.WriteLine();
                         Console.WriteLine("You won! As we know, scissors beats paper.");
                         Console.WriteLine();
-                        Console.WriteLine("You won your bet back which was " + bet + " Dollars");
+                        Console.WriteLine("You won your bet!");
                         wallet = wallet + bet;
                     }
                     else if (randNum == 3)
@@ -160,14 +157,27 @@ namespace Topic_1_7_Summative
                         Console.WriteLine("You tied! As we know, scissors can't beat scissors.");
                     }
                 }
-                Console.WriteLine();
-                Console.WriteLine("Type 'y' to play again and press 'Enter' if you want to quit");
-                Console.WriteLine();
-                choise = Console.ReadLine().ToLower();
-                if (choise != "y")
+                if (wallet == 0)
                 {
-                    Console.WriteLine("Thank you and have a nice day! :)");
+                    Console.WriteLine();
+                    Console.WriteLine("Sorry you ran out of money, please try again later, Ok?");
                     done = true;
+                }
+                else if (wallet != 0)
+                {
+
+                    Console.WriteLine();
+                    Console.WriteLine("Type 'y' to play again or press 'Enter' if you want to quit");
+                    Console.WriteLine();
+                    choise = Console.ReadLine().ToLower();
+                    Console.Clear();
+                    if (choise != "y")
+                    {
+                        Console.WriteLine("Thank you and have a nice day! :)");
+                        Console.WriteLine();
+                        Console.WriteLine("You ended with " + wallet + " in your bank.");
+                        done = true;
+                    }
                 }
             }
         }
